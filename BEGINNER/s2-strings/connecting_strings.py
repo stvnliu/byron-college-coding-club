d9e633df49f7834b1285c42be52c003279974718
Justin He <74974216+Ateee329@users.noreply.github.com>
2023-10-02 00:12:26 +0300

    Create connecting_strings.py

---
+++ BEGINNER/s2-strings/connecting_strings.py
@@ -0,0 +1,13 @@
+test1 = 'Hello'
+test2 = 'World'
+
+test3 = test1 + test2
+print(test3)
+# expected result: HelloWorld
+
+test4 = test1 + ' ' + test2
+print(test4)
+# expected result: Hello World
+
+print(test1, test2)
+# expected result: Hello World
