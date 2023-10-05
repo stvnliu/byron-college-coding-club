60fd84652cba80d72f6496d28d77cabd44535031
Justin He <74974216+Ateee329@users.noreply.github.com>
2023-10-05 13:57:25 +0300

    Create access_list.py
    create and update

---
+++ BEGINNER/s4-list_and_set/access_list.py
@@ -0,0 +1,15 @@
+list = ["apple", "banana", "peach", "watermelon"]
+num = 1
+print(list[num])
+# in python, everything starts from 0
+# so print(list[1]) will be the second element in the list
+# and print(list[0]) will be the first element in the list
+print(list[-1])
+# this will return the last element in the list as what we've metioned in string
+print(list[1:3])
+# will start at index 1 (included) and end at index 3 (not included)
+print(list[:1])
+# print to the index 1 element (not included)
+print(list[2:])
+# print from the index 2 element (included)
+# all of them work in the same way in negative number
