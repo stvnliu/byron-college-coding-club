426f07f28b14cd35b4edd0e93d21b5dff940c147
Justin He <74974216+Ateee329@users.noreply.github.com>
2023-10-06 15:04:57 +0300

    Update modify_list.py

--- BEGINNER/s4-list/modify_list.py
+++ BEGINNER/s4-list/modify_list.py
@@ -1,1 +1,24 @@
+data = [0, 56, 12, 3556, 6787, 114514, 1919810]
 
+data[0] = 100
+print(data)
+
+data[1:3] = [1, 2]
+print(data)
+
+data[4:6] = [343]
+print(data)
+# If you insert less items than you replace,
+# the new items will be inserted where you specified, 
+# and the remaining items will move accordingly
+
+data[1:2] = [5, 6, 7, 8]
+print(data)
+# If you insert more items than you replace, 
+# the new items will be inserted where you specified,
+# and the remaining items will move accordingly
+
+# insert item
+data.insert(0, '90')
+print(data)
+# the position of inserted element is before the index that it is inputted
