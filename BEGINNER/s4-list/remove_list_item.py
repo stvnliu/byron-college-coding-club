86573f1e814590680931322892a9978d7d226199
Justin He <74974216+Ateee329@users.noreply.github.com>
2023-10-10 09:18:04 +0300

    Update remove_list_item.py

--- BEGINNER/s4-list/remove_list_item.py
+++ BEGINNER/s4-list/remove_list_item.py
@@ -1,1 +1,16 @@
+fruits = ["Apple", "Banana", "Peach"]
+fruits1 = fruits
 
+fruits.remove("Peach")
+print(fruits)
+# This remove a specific element that you want
+
+fruits.pop(0)
+print(fruits)
+# This will delete the element with coresponding index
+
+del fruits1[0]
+# This will delete the element with coresponding index too
+
+del fruits1
+# This will delete the whole list
