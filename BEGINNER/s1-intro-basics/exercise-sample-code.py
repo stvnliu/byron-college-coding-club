656f998b38e8cf265723c3014ce137e50f501271
Justin He <74974216+Ateee329@users.noreply.github.com>
2023-10-30 17:23:13 +0200

    Create exercise-sample-code.py
    create sample code for the exercise in readme

---
+++ BEGINNER/s1-intro-basics/exercise-sample-code.py
@@ -0,0 +1,12 @@
+# Input from the user
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+email = input("Please enter your email address: ")
+gender = input("Please enter your gender: ")
+
+# Displaying user information
+print("\n----- Your Information -----")
+print("Name:", name)
+print("Age:", age)
+print("Email:", email)
+print("Gender:", gender)
