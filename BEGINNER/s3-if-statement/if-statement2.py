4b9dde83f4cc55af4a0f3b18bfe3b210227cc9a6
Justin He <justin.he814@gmail.com>
2023-11-20 13:53:39 +0200

    Create if-statement2.py

---
+++ BEGINNER/s3-if-statement/if-statement2.py
@@ -0,0 +1,14 @@
+# Here is the example that we did during the session
+cont = input("Continue? [y/N]")
+while cont == "y":
+  print("loopty lop")
+  cont = input("Continue? [y/N]")
+  while cont == "N":
+    cont = input("Are you sure about that? [y/N]")
+    if cont = y:
+      continue
+    elif cont = N:
+      break
+    else:
+      print("Please enter 'y' or 'N'")
+  
