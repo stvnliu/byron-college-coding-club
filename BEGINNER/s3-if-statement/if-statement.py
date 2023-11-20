99a1aac7358b19411bd580936aa3d63e522a75ed
Justin He <justin.he814@gmail.com>
2023-11-20 13:39:04 +0200

    Update if-statement.py

--- BEGINNER/s3-if-statement/if-statement.py
+++ BEGINNER/s3-if-statement/if-statement.py
@@ -1,8 +1,21 @@
-a = 1
-b = 2
-if a > b:
-  print("a is bigger.")
-elif b > a:
-  print("b is bigger.")
+# Python if statement
+pathChoice = True
+
+choice = input("Enter 0 or 1: ")
+if choice == "1":
+  pathChoice = True
+elif choice == "0":
+  pathChoice = False
 else:
-  print("a and b are equal.")
+  while choice != "path1" and choice != "path2":
+    choice = input("Choice invalid, please enter anagin")
+    
+ouputResult = ""
+
+if pathChoice == True:
+  print("Path choice is 1")
+  outputResult = "Result of path 1"
+else:
+  print("Path choice is 2")
+  outputResult = "Result of path 2"
+print(ouputResult)
